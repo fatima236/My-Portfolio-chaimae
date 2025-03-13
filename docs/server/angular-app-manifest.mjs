2960b6a878a30729d243453c20405e278e63bd07
chaimaeb627 <chaimaeb627@gmail.com>
2025-03-13 23:47:29 +0000

    fin

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -8,13 +8,18 @@ export default {
   {
     "renderMode": 2,
     "route": "/My-Portfolio"
+  },
+  {
+    "renderMode": 2,
+    "redirectTo": "/My-Portfolio",
+    "route": "/My-Portfolio/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1931, hash: '57ddb951425c320d7482a241bc2066d4f49f3bc705ba9d1cc69cebb681e06035', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2399, hash: '65e080faf5692a6cdeb938036fd9d5268b6c6864722d779155c3a94d1868dbe9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 55329, hash: 'ba223cd1cda8e707c8903cddf59fed30410cee15734cf7b32ce3e877f1c41260', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1931, hash: '1c9e3b2db6b1c5076269586bd8bd6fe0924e73bf0143fce3dad6808f9a8df110', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2399, hash: 'acb44c7f12784478d3c113787fe0a7a316517d5c98d9fdb646fa5193ff61680e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 57333, hash: 'aa72eec29f0991539e63fcfa3568c15286e394f6a1d17caeb22e0759c4cde7a0', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-PKANYXH2.css': {size: 46, hash: 'AAKvqYnWG2M', text: () => import('./assets-chunks/styles-PKANYXH2_css.mjs').then(m => m.default)}
   },
 };
